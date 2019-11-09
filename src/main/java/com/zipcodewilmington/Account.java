c2d21c127cf4c3e63434d4bd50a0ece1086bb3c9
wes <wes.jones1597@gmail.com>
2019-11-09 15:23:55 -0500

    account tests passing

--- src/main/java/com/zipcodewilmington/Account.java
+++ src/main/java/com/zipcodewilmington/Account.java
@@ -3,14 +3,20 @@ package com.zipcodewilmington;
 import java.util.ArrayList;
 
 public class Account {
-    private Double balance;
+    public Double balance;
     private Integer userId;
     private Integer accountId;
     private Boolean accountIsOpen = false;
     private ArrayList<String> transactionHistory;
     private static final String[] accountNames = {"Checking", "Savings", "Investments"};
 
-
+    public Account(Double balance) {
+        this.balance = balance;
+        this.userId = 0;
+        this.accountId = 0;
+        this.accountIsOpen = true;
+        this.transactionHistory = new ArrayList<String>();
+    }
     public Account(Double balance, Integer userId, Integer accountId) {
         this.balance = balance;
         this.userId = userId;
@@ -64,6 +70,41 @@ public class Account {
     public Integer getAccountId() { return this.accountId; }
 
     public String getName() { return accountNames[this.accountId];}
+
+    public Boolean closeAccount() {
+        Boolean closed = false;
+        if (!(this.balance() > 0.00)) {
+            closed = true;
+        }
+        return closed;
+    }
+
+    public Double balance() {
+        return this.balance;
+    }
+
+    public void deposit(Double amount) {
+        if (amount > 0.0) {
+            Double newBal = this.balance() + amount;
+            this.setBalance(newBal);
+        }
+    }
+
+    public Double withdraw(Double amount) {
+        Double newBal = 0.0;
+        if (this.balance() >= amount && amount > 0.0) {
+            newBal = this.balance() - amount;
+            this.setBalance(newBal);
+        }
+        return newBal;
+    }
+
+    public void transfer(Account target, Double amount) {
+        if (this.balance() >= amount && amount > 0.0) {
+            target.deposit(amount);
+            this.withdraw(amount);
+        }
+    }
 
 
 }
